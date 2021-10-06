8c7f8b7bfb1dd5fc43509360e65611d57fb2f74d
kevin mejia <kevineliasmejia@gmail.com>
2021-10-06 17:16:02 -0600

    Subida de archivos

---
+++ Conexion/login.php
@@ -0,0 +1,7 @@
+<?php 
+	define("HOSTNAME", "localhost");// Nombre del host
+	define("DATABASE", "ejemplo1"); // Nombre de la base de datos
+	define("USERNAME", "mm17096"); // Nombre del usuario
+	define("PASSWORD", "rootmm17096");
+
+?>
